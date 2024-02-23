9a344a9c0178020e939e38243e3941041ed8deec
Akash Shahrar <nasib.akash@portonics.com>
2024-02-23 21:53:50 +0600

    first commit

---
+++ backend/db/index.ts
@@ -0,0 +1,69 @@
+import { Db, MongoClient, ServerApiVersion } from 'mongodb';
+
+import {
+    findOneFromDB as _findOneFromDB, insertInDb as _insertInDb, updateOneFromDb as _updateOneFromDb
+} from './inMemDB';
+
+const uri = process.env.MONGO_URI;
+const dbName = process.env.MONGO_DB_NAME;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+console.debug("The uri "+uri);
+
+
+let db: Db = null;
+let inMem = uri ? false : true;
+
+const connectDb = async (): Promise<void> => {
+  try {
+    if (!client) throw new Error("No client");
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error({ err })
+    inMem = true;
+    // eslint-disable-next-line no-console
+    console.error("MONGO DB ERROR!", "Using in-memory DB - Not reliable!!");
+  }
+};
+
+const insertInDb = async<T>(data: T, collectionName: string): Promise<T> => {
+  if(inMem) {
+    _insertInDb(data, collectionName);
+  }else {
+    await db.collection(collectionName).insertOne(data);
+  }
+
+  return data;
+};
+
+const findOneFromDB = async<T>(findCondition, collectionName: string): Promise<T> => {
+  if(inMem) {
+    return _findOneFromDB(findCondition, collectionName);
+  }
+
+  return db.collection(collectionName).findOne(findCondition) as T;
+}
+
+const updateOneFromDb = async<T>(condition, data, collectionName: string): Promise<T> => {
+  if(inMem) {
+    return _updateOneFromDb(condition, data, collectionName) as Promise<T>;
+  }
+  return db.collection(collectionName).updateOne(condition, { $set: data })  as Promise<T>;
+}
+
+export default {
+  db,
+  connectDb,
+  insertInDb,
+  findOneFromDB,
+  updateOneFromDb,
+};
